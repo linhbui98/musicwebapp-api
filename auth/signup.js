ae46ec380f2b3a1e61d4d6d85d90cbb8e4dd6940
builinh98 <linhbq.intern@gmail.com>
2020-05-26 15:41:56 +0700

    fix signup

--- auth/signup.js
+++ auth/signup.js
@@ -9,8 +9,8 @@ module.exports.signupHandle = async (req, res) => {
     const { email, username, password, fullName } = req.body
     const user = await User.findOne({ $or:[{ email: email}, {username: username }] })
     // Make sure user doesn't already exist
-    if (user && user.email === email) return res.status(400).send({ msg: 'The email address you have entered is already associated with another account.' });
-    if (user && user.username === username) return res.status(400).send({ msg: 'The username you have entered is already associated with another account.' });
+    if (user && user.email === email) return res.status(200).send({ msg: 'The email address you have entered is already associated with another account.' });
+    if (user && user.username === username) return res.status(200).send({ msg: 'The username you have entered is already associated with another account.' });
 
     // Create and save the user
     const newUser = new User({
